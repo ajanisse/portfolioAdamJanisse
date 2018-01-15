8333d2b97bc1793e5ff12df894fc48508eda6dea
Adam <ajanisse@gmail.com>
2018-01-15 05:32:12 -0500

    Cleanup up for presentation

--- janisse-adam-portfolio-final/js/contentObject.js
+++ janisse-adam-portfolio-final/js/contentObject.js
@@ -1,8 +1,5 @@
 var dynamicContent = {
-		spring: {
-			text : "Spring is one of the four conventional temperate seasons, following winter and preceding summer. There are various technical definitions of spring, but local usage of the term varies according to local climate, cultures and customs. When it is spring in the northern hemisphere, it will be autumn in the southern hemisphere. At the spring equinox, days are close to 12 hours long with day length increasing as the season progresses. Spring and 'springtime' refer to the season, and also to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth.",
-
-			headline : "Spring",
+		gallery: {
 
 			images : [
 				"hex-movie.png",
